54df094b7214d804fe109a392bf01a297d8a166d
Armin Kazmi <armin.kazmi@tu-dortmund.de>
2012-01-04 16:37:16 +0100

    Change build.sh in order to use the submodule

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-rdmd --build-only -ofbin/orb -m32 -Iorange -L-lruby.1.9.1-static -J./resources -L-ltango -L-lz "$@" orbit/orb/Orb.d
+rdmd --build-only -ofbin/orb -m32 -Iorange -L-lruby.1.9.1-static -J./resources -L-LTango-D2 -ITango-D2 -L-ltango -L-lz "$@" orbit/orb/Orb.d
